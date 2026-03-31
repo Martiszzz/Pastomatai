2fc81d3f146b16b576f75008374825ac4eec2be2
Martiszzz <114743025+Martiszzz@users.noreply.github.com>
2026-03-31 17:40:14 +0300

    hello world

---
+++ Front/src/index.tsx
@@ -0,0 +1,13 @@
+
+import styles from "./index.module.css" 
+function Testas(){
+    return (
+        <>
+        <div className={styles.title}>
+            <h1>Hello worldas</h1>
+        </div>
+        </>
+    )
+}
+
+export default Testas
